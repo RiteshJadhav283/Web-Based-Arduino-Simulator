f0f878271408468a0adb7857097068aef92bc80d
Ritesh Jadhav <ritesh6798@hotmail.com>
2026-01-26 23:26:13 +0530

    day1

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import Navigation from './components/Navigation'
+import Utilities_Bar from './components/Utilities_Bar'
+
+function App() {
+  return (
+    <>
+      {/* <Navigation/> */}
+      <Utilities_Bar/>
+    </>
+  )
+}
+
+export default App
